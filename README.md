98be15f106b40dedf81063f94cdaf2465e56b101
Rogério Amorim <rtechdev.pt@gmail.com>
2023-09-22 01:59:11 -0300

    desafio-if-else

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # dio-log-desafios
-Desafios Lógica de Programação
+Desafios Logica de Programacao
+
+    ### 1 Desafio Classificador de nivel de herois
+    - variaveis;
+        string, integer, boolean
+    - operadores;
+        + - * / %
+        && || !
+        <> == === 
+    - laços de repetiçoes
+        for while do while
+    - estrutura de decisoes
+        if else else if , switch case
+
+    ### Objetivo do desafio:
+    Crie uma variavel para armazenar o nome de um heroi e a quantidade de experiencia (xp) de um heroi, depois utilize uma estrutura de descisao para apresentar alguma das mensagens abaixo:
+
+        - XP for menor do que 1.000 = Ferro
+        - XP for entre 1.001 e 2.000 = Bronze
+        - XP for entre 2.001 e 5.000 = Prata
+        - XP for entre 5.001 e 7.000 = Ouro
+        - XP for entre 7.001 e 8.000 = Platina Diamante
+        - XP for entre 8.001 e 9.000 = Ascendente
+        - XP for entre 9.001 e 10.000 = Imortal
+        - XP for maior ou igual a 10.001 = Radiante
+
+    ### Saída
+
+        Ao final deve exibir uma mensagem:
+
+        "O Herói de nome XXXXXXX está no nível de XXXXXXX"
+
+
--- script.js
+++ script.js
@@ -1,2 +1,48 @@
-let teste = "Olá Mundo!!!"
-console.log(teste)
+// caso seja passado uma posição inválida do array o programa quebra, como tratar?
+// dentro do switch case existe uma forma de utilizar condiçoes?
+
+let marvelHero = [
+    ["Batman",  7.500],   //0
+    ["Superman", 9.500],   //1
+    ["Spiderman", 7.300],   //2
+    ["Ironman",  8.200]    //3
+];
+let nivelHero
+let heroChoose = marvelHero[2]
+
+
+    if (heroChoose[0,1] < 1.000){
+        nivelHero = "Ferro"
+        console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+    } 
+        else if (heroChoose[0,1] > 1.000 && heroChoose[0,1] <= 2.000){
+            nivelHero = "Bronze"
+            console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+        } 
+            else if (heroChoose[0,1] > 2.000 &&  heroChoose[0,1] <= 5.000){
+                nivelHero = "Prata"
+                console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+            }
+                else if (heroChoose[0,1] > 5.000 &&  heroChoose[0,1] <= 7.000){
+                    nivelHero = "Ouro"
+                    console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+                }
+                    else if (heroChoose[0,1] > 7.000 &&  heroChoose[0,1] <= 8.000){
+                        nivelHero = "Platina"
+                        console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+                    }
+                        else if (heroChoose[0,1] > 8.000 &&  heroChoose[0,1] <= 9.000){
+                            nivelHero = "Ascedente"
+                            console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+                        }
+                            else if (heroChoose[0,1] > 9.000 &&  heroChoose[0,1] <= 10.000){
+                                nivelHero = "Imortal"
+                                console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+                            }
+                                else if (heroChoose[0,1] > 10.000){
+                                    nivelHero = "Radiante"
+                                    console.log("The hero name is " + heroChoose[0].toUpperCase() + " and your nivel is " + nivelHero)
+                                }
+         else{
+                console.log("ALERT: Your level is small, You shall start again.")
+            }
